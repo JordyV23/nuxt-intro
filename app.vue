b39864ceed096e6678e80b9f812d2fe5bd01b712
JordyV23 <jordyvargas0123@gmail.com>
2023-09-03 20:10:44 -0600

    Cambios realizados: -Exploracion de uso de componentes -Exploracion de uso de paginas -Exploracion de uso de router y parametros

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    Hello nuxt 3
+    <!-- Se debe de implementar NUXTLayout junto con NuxtPage  -->
+    <!-- Esto para acceder al router que tiene nuxt -->
+    <NuxtLayout>
+      <Navbar/>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
+
+<style>
+
+body{
+  background: #101423;
+  color: white;
+}
+
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <ul>
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/events">Events</NuxtLink></li>
+      <li><NuxtLink to="/events/profile">Profile</NuxtLink></li>
+    </ul>
+  </header>
+</template>
