a469d9fa631c822215154af40510ae0dd8fcc8ec
JordyV23 <jordyvargas0123@gmail.com>
2023-09-05 18:10:08 -0600

    Cambios realizados: -Exploracion de uso de composables

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
+<script setup>
+//Gracias NUXT se realizan automaticamente las importaciones
+//Siempre y cuando se encuentren en la carpeta composables
+const { sayHello } = useUtils();
+sayHello();
+</script>
+
 <template>
-    <div>
-        <Alert/>
-    </div>
+  <div>
+    <Alert />
+  </div>
 </template>
